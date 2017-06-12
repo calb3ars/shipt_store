6e2727c38a8ea878a7a8b566d45e1e399f9f0be2
Andrew Jiang <andrew@Andrews-MacBook-Pro.local>
2017-06-12 16:00:51 -0700

    Seed data added for Customers and Orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+customers = Customer.create([
+  {
+    first_name: "test",
+    last_name: "user"
+  },
+  {
+    first_name: "Andrew",
+    last_name: "Jiang"
+  }
+])
+
+orders = Order.create([
+  {
+    customer_id: 1,
+  },
+  {
+    customer_id: 1,
+    status: "On the Way"
+  },
+  {
+    customer_id: 1,
+    status: "Delivered"
+  },
+  {
+    customer_id: 1,
+    status: "Delivered"
+  },
+  {
+    customer_id: 2,
+    status: "Delivered"
+  },
+  {
+    customer_id: 2,
+    status: "On the Way"
+  },
+  {
+    customer_id: 2,
+    status: "On the Way"
+  }
+])
