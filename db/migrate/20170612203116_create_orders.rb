0adae9f00ca52fe1dececc3de5b3dd5db8bebf86
Andrew Jiang <andrew@Andrews-MacBook-Pro.local>
2017-06-12 13:36:15 -0700

    Orders migration

---
+++ db/migrate/20170612203116_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :orders do |t|
+      t.integer :customer_id, null: false
+      t.string :status, null: false, default: "Waiting for Delivery"
+
+      t.timestamps null: false
+    end
+    add_index :orders, :customer_id, unique: true
+  end
+end
